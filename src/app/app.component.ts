5d6a77ef1f5f8bff47949627bc17fc210e5ec163
fmauff1978 <fmauff1978@gmail.com>
2022-03-25 15:08:54 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = "My first Angular App";
+export class AppComponent {
+  name = 'My first Angular App';
 }
